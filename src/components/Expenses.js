dfa3a7155b5ff4ac6cfd8c8e3719770ed40cc61f
kuusmaa <4j534j@gmail.com>
2024-01-08 21:23:01 +0200

    yl2

---
+++ src/components/Expenses.js
@@ -0,0 +1,14 @@
+import "./Expenses.css"
+import ExpenseItem from './ExpenseItem'
+import Card from "./Card";
+
+function Expenses(props){
+	return(
+		<Card className="expenses">
+			<ExpenseItem expenseData={props.expensesData[0]}></ExpenseItem>
+        	<ExpenseItem expenseData={props.expensesData[1]}></ExpenseItem>
+		</Card>
+		)
+}
+
+export default Expenses
